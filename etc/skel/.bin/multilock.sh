f2046e75627953bafdb5c037966053d4acd23685
creio <mailcreio@gmail.com>
2025-02-23 22:36:01 +0300

    init commit

---
+++ etc/skel/.bin/multilock.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+IMG=/usr/share/wall/wl.png
+
+if [[ $(command -v  betterlockscreen) ]]; then
+  if [[ ! -d $HOME/.cache/betterlockscreen ]]; then
+    betterlockscreen -u $IMG --blur 0.5
+  fi
+
+  betterlockscreen $1 $2
+fi
